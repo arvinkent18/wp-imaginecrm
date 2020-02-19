f2d862132a8367214fcf80563d1b17428346e27e
Von Moqthar Sapaih <artex07@gmail.com>
2020-02-19 17:21:08 +0800

    Added footer section

--- footer.php
+++ footer.php
@@ -1,4 +1,11 @@
 <footer>
+<div class="grid">
+    <div class="row" id="footer-sect">
+        <div class="col-md-12">
+            Test
+        </div>
+    </div>
+</div>
 </footer>
 <?php wp_footer(); ?>
 </body>
