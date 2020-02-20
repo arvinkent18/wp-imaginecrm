a79d89052e6c292b3bbc44cc326c10347517d37b
Von Moqthar Sapaih <artex07@gmail.com>
2020-02-20 14:37:33 +0800

    footer added

--- footer.php
+++ footer.php
@@ -1,13 +1,49 @@
 <footer>
 <div class="container-fluid">
     <div class="grid w-100">
-        <div class="row no-gutters w-100" id="footer-sect">
+        <div class="row no gutters w-100 h-200" id = "subscribe-sect">
             <div class="col-md-12">
-                Test
+            <h2 class=" text-center">
+                <span class="breaker">SUBSCRIBE</span>
+            </h2>
+            <p style = "text-align:center; color: grey;">Sign up with your email address to receive news and updates</p>
+        
             </div>
         </div>
-    </div>
+
+
+
+        <div class="row no-gutters w-100" id="footer-sect">
+            <div class="col-md-8">
+            <ul class="fa-ul" id="lista">
+            <li> <a href="https://www.w3schools.com"><i style="color: white;" class="fa fa-facebook-f" id = "icons"></i></a></li>
+            <li><i style="color: white;" class="fa fa-linkedin-square" id = "icons"></i></li>
+            <li><i class="fa fa-twitter  fa-inverse" id = "icons"></i></li>
+            </ul>
+            
+                <div class="flex-column">
+                <span class="navbar-text phone-numbers" id = "contact">(213) 632-6840INFO@IMAGINECRM.ORG</span>
+                </div>
+            
+                <div class="flex-column">
+                <span class="navbar-text phone-numbers" id = "terms" >DIVERSITY - RELATIONSHIPS - EDUCATION</span>
+                </div>
+            
+                <div class="flex-column">
+                <span class="navbar-text phone-numbers" id = "copyright" >COPYRIGHT<span><i class="fa fa-copyright" aria-hidden="true"></i></span> 2019 imagineCRM. All rights reserved.</span>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <img src="/wordpress/wp-content/themes/wp-imaginecrm/assets/bottomlogo2.png" alt="logo" width="auto" height="300" class = "responsive" id = "botlogo">
+            </div>
+
+        </div>
+    
+
+           
 </div>
+</div>
+        
 
 </footer>
 <?php wp_footer(); ?>
--- header.php
+++ header.php
@@ -17,6 +17,7 @@
 
     <meta name="viewport" content="width=device-width">
     <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
+    
 
     <?php wp_head(); ?>
 </head>
